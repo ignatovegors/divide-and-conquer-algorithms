c8fee4666eaefdea70ecfafba2bf1b2d30a57860
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-27 14:00:42 +0300

    Add files via upload

---
+++ inversions-number.py
@@ -0,0 +1,53 @@
+def inversions_number(lst):
+    inv_counter = 0
+
+    while len(lst) > 1:
+
+        if len(lst) % 2 == 1:
+            lst.append([10**10])
+
+        temp = []
+
+        for k in range(0, len(lst), 2):
+            left = lst[k]
+            right = lst[k + 1]
+            res = []
+            i = 0
+            j = 0
+
+            while True:
+                if i == len(left):
+                    res += right[j::]
+                    break
+                elif j == len(right):
+                    res += left[i::]
+                    break
+
+                if left[i] > right[j]:
+                    inv_counter += len(left) - i
+                    res.append(right[j])
+                    j += 1
+                else:
+                    res.append(left[i])
+                    i += 1
+
+            temp.append(res)
+
+        lst = temp
+
+    return inv_counter
+
+
+def main():
+    lst = []
+
+    n = int(input())
+
+    for element in input().split():
+        lst.append([int(element)])
+
+    print(inversions_number(lst))
+
+
+if __name__ == '__main__':
+    main()

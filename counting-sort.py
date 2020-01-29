e4c42a2c89a1b14b21ef9acf35d63363c7c504d5
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-29 23:12:40 +0300

    Create counting-sort.py

---
+++ counting-sort.py
@@ -0,0 +1,29 @@
+def counting_sort(lst, n):
+    temp = [0] * n
+    res = [0] * len(lst)
+
+    for element in lst:
+        temp[element] += 1
+
+    for i in range(1, n):
+        temp[i] += temp[i - 1]
+
+    for j in range(len(lst) - 1, -1, -1):
+        res[temp[lst[j]] - 1] = lst[j]
+        temp[lst[j]] -= 1
+
+    return res
+
+
+def main():
+    n = int(input())
+    a = []
+
+    for element in input().split():
+        a.append(int(element))
+
+    print(*counting_sort(a, 11))
+
+
+if __name__ == '__main__':
+    main()
